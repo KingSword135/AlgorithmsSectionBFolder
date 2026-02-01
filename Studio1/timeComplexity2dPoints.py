5d79251824f53d55ea6b23c52346b26dd09af8aa
Matthew Blanchard <cheesered103@gmail.com>
2026-02-01 16:38:30 -0500

    Added time complexity for graph thing

---
+++ Studio1/timeComplexity2dPoints.py
@@ -0,0 +1,36 @@
+import numpy as np, matplotlib.pyplot as plt
+import ClosestPointPairs
+import random, time
+
+def fillRandom(point_array):
+    for i in range(0, i):
+        sublist = []
+        sublist.append(random.random(-20,20))
+        sublist.append(random.random(-20,20))
+        point_array
+    
+
+input_sizes = np.linspace(2,302,300,dtype=int)
+times = []
+
+for i in input_sizes:
+
+    points_array = []
+    for i in range(0, i):
+        sublist = []
+        sublist.append(random.uniform(-20,20))
+        sublist.append(random.uniform(-20,20))
+        points_array.append(sublist)
+
+    start = time.time()
+    ClosestPointPairs.getMinDistance(points_array)
+    end = time.time()
+    times.append(end - start)
+
+plt.figure(figsize=(8,5))
+plt.plot(input_sizes,times,label='O(n^2) 2D Closest Point',color='green')
+plt.xlabel("Input Size (n)")
+plt.ylabel("Time Required (seconds)")
+plt.legend()
+plt.grid(False)
+plt.show()
